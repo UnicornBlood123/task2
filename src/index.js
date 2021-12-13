538e3f713da501c8f586ef545af252a3741af11f
UnicornBlood123 <gogulin.vova@mail.ru>
2021-12-13 15:06:01 +0300

    second commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import сalculator from "./сalculator.js"
+import commands from "./commands.js"
+import invoker from "./invoker.js"
+import client from "./client.js"
+import './style.css';
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        main:path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/index.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+}
